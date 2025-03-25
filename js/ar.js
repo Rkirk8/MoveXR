b96cb08f374f5c36c36d2efdd3824c3ef6973bdd
Rielly Kirk <Riellykirk13@gmail.com>
2025-03-25 00:01:16 -0400

    made some more changes to make 2&3 closer and on the floor and lowerd 4

--- js/ar.js
+++ js/ar.js
@@ -49,25 +49,24 @@ const createScene = async function () {
   const defaultBoxMaterial = new BABYLON.StandardMaterial("defBoxMat", scene);
   defaultBoxMaterial.diffuseColor = new BABYLON.Color3(1, 0, 0); // Red color
 
-
-
+  // x right/left, y height, z depth
   //box 1 (duck)
   const box1 = BABYLON.MeshBuilder.CreateBox("box1", { height: .5, width: 2, depth: 1 }, scene);
   box1.position = new BABYLON.Vector3(0.2, 1.7, 2);
   box1.material = defaultBoxMaterial;
   //box 2 (step left)
   const box2 = BABYLON.MeshBuilder.CreateBox("box2", { height: 3, width: 1.5, depth: 1 }, scene);
-  box2.position = new BABYLON.Vector3(1.5, 0.5, 4.5);
+  box2.position = new BABYLON.Vector3(1, 0.5, 4.5);
   box2.material = defaultBoxMaterial;
 
   //box 3 (step right)
   const box3 = BABYLON.MeshBuilder.CreateBox("box3", { height: 3, width:1.5, depth: 1 }, scene);
-  box3.position = new BABYLON.Vector3(-1.5, 0.5, 7);
+  box3.position = new BABYLON.Vector3(-1, 1, 7);
   box3.material = defaultBoxMaterial;
 
   //box 4 (jump)
   const box4 = BABYLON.MeshBuilder.CreateBox("box4", { height: .25, width: 2, depth: .5 }, scene);  
-  box4.position = new BABYLON.Vector3(0, 0.5, 9.5);
+  box4.position = new BABYLON.Vector3(0, .123, 9.5);
   box4.material = defaultBoxMaterial;
 
   /* MOVE BOXES FORWARD & LOOP */
